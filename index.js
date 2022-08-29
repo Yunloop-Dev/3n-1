6ef31867d179f17aed844a727534311cb9cdd63b
Yunloop-Dev <33457869+Yunloop-Dev@users.noreply.github.com>
2022-08-29 16:03:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const threeNPlusOne = (index) => {
+  let result = index;
+
+  let iterations = 0;
+
+  console.time("Время обработки:");
+
+  while (result !== 1) {
+    if (result % 2 === 0) {
+      result = result / 2;
+    } else {
+      result = result * 3 + 1;
+    }
+    iterations++;
+  }
+  console.log(
+    `\n\nОтчёт:\n\nВводное число: ${index}\nВсего итераций 4/2/1: ${iterations}`
+  );
+  console.timeEnd("Время обработки:");
+};
+
+threeNPlusOne(500000);
